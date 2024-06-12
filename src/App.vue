20c308e1a9f979172cb332957db9ba0060a72f62
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2024-06-12 21:17:17 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ const weather = ref({
 <template>
   <div class="wrapper">
     <h1>Погодное приложение</h1>
-    <P>Узнать погоду в {{ city }}</P>
+    <P>Узнать погоду в {{ city == "" ? "вашем городе" : city }}</P>
     <input type="text" v-model="city" placeholder="Введите город">
     <button>Получить погоду</button>
   </div>
