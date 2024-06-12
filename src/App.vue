09628e2715d24215db63bd24ad68842cfc408ff3
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2024-06-12 21:04:44 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script>
-
+export default {
+  data() {
+    return {
+      city: ""
+    }
+  }
+}
 </script>
 
 <template>
   <div class="wrapper">
     <h1>Погодное приложение</h1>
-    <P>Узнать погоду в вашем городе</P>
+    <P>Узнать погоду в {{ city }}</P>
+    <input type="text" @input="this.city = $event.target.value" placeholder="Введите город">
+    <button>Получить погоду</button>
   </div>
 </template>
 
