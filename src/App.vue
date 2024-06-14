b7094d92c5cb26019b7d61a0062285833c46242b
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2024-06-14 15:18:36 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 const weather = ref({
   data() {
     return {
       city: ""
+    }
+  },
+  computed: {
+    cityName() {
+      return "«" + this.city + "»"
+    }
+  },
+  methods: {
+    getWeather() {
+      alert("Some")
     }
   }
 })
@@ -13,9 +23,9 @@ const weather = ref({
 <template>
   <div class="wrapper">
     <h1>Погодное приложение</h1>
-    <P>Узнать погоду в {{ city == "" ? "вашем городе" : city }}</P>
-    <input type="text" v-model="city" placeholder="Введите город">
-    <button v-if="city != ''">Получить погоду</button>
+    <p>Узнать погоду в {{ weather.city == "" ? "вашем городе" : computed.cityName }}</p>
+    <input type="text" v-model="weather.city" placeholder="Введите город">
+    <button v-if="weather.city != ''" @click="getWeather()">Получить погоду</button>
     <button disabled v-else>Введите название города</button>
   </div>
 </template>
